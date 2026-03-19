acbf09a9e14d0081cd5256aa2e125cffcc4181ea
Webprojeto <github@webprojeto.com.br>
2026-03-19 10:20:17 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,6 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/',
+  base: process.env.VITE_BASE_URL
+    ?? (process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/'),
 })
